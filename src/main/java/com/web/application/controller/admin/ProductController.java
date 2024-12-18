f705db53cf29e08516f8d100dc9756bd726d58de
DucPM1206 <ducphamminh1206@gmail.com>
2024-12-18 22:58:03 +0700

    Update

--- src/main/java/com/web/application/controller/admin/ProductController.java
+++ src/main/java/com/web/application/controller/admin/ProductController.java
@@ -1,26 +1,9 @@
 package com.web.application.controller.admin;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.file.Files;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
-import org.apache.poi.ss.usermodel.CellStyle;
-import org.apache.poi.ss.usermodel.CreationHelper;
-import org.apache.poi.ss.usermodel.FillPatternType;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFCellStyle;
-import org.apache.poi.xssf.usermodel.XSSFColor;
-import org.apache.poi.xssf.usermodel.XSSFFont;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -52,17 +35,11 @@ import com.web.application.service.ImageService;
 import com.web.application.service.ProductService;
 
 import jakarta.servlet.ServletContext;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
 public class ProductController {
-
-	private String xlsx = ".xlsx";
-	private static final int BUFFER_SIZE = 4096;
-	private static final String TEMP_EXPORT_DATA_DIRECTORY = "\\resources\\reports";
-	private static final String EXPORT_DATA_REPORT_FILE_NAME = "San_pham";
 
 	@Autowired
 	private ServletContext context;
@@ -210,148 +187,5 @@ public class ProductController {
 		productService.updatefeedBackImages(id, req);
 
 		return ResponseEntity.ok("Cập nhật thành công");
-	}
-
-	@GetMapping("/api/products/export/excel")
-	public void exportProductDataToExcelFile(HttpServletResponse response) {
-		List<Product> result = productService.getAllProduct();
-		String fullPath = this.generateProductExcel(result, context, EXPORT_DATA_REPORT_FILE_NAME);
-		if (fullPath != null) {
-			this.fileDownload(fullPath, response, EXPORT_DATA_REPORT_FILE_NAME, "xlsx");
-		}
-	}
-
-	private String generateProductExcel(List<Product> products, ServletContext context, String fileName) {
-		String filePath = context.getRealPath(TEMP_EXPORT_DATA_DIRECTORY);
-		File file = new File(filePath);
-		if (!file.exists()) {
-			new File(filePath).mkdirs();
-		}
-		try (FileOutputStream fos = new FileOutputStream(file + "\\" + fileName + xlsx);
-				XSSFWorkbook workbook = new XSSFWorkbook();) {
-
-			XSSFSheet worksheet = workbook.createSheet("Product");
-			worksheet.setDefaultColumnWidth(20);
-
-			XSSFRow headerRow = worksheet.createRow(0);
-
-			XSSFCellStyle headerCellStyle = workbook.createCellStyle();
-			XSSFFont font = workbook.createFont();
-			font.setFontName(XSSFFont.DEFAULT_FONT_NAME);
-			font.setColor(new XSSFColor(java.awt.Color.WHITE));
-			headerCellStyle.setFont(font);
-			headerCellStyle.setFillForegroundColor(new XSSFColor(new java.awt.Color(135, 206, 250)));
-			headerCellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-
-			XSSFCell productId = headerRow.createCell(0);
-			productId.setCellValue("Mã sản phẩm");
-			productId.setCellStyle(headerCellStyle);
-
-			XSSFCell productName = headerRow.createCell(1);
-			productName.setCellValue("Tên sản phẩm");
-			productName.setCellStyle(headerCellStyle);
-
-			XSSFCell productBrand = headerRow.createCell(2);
-			productBrand.setCellValue("Thương hiệu");
-			productBrand.setCellStyle(headerCellStyle);
-
-			XSSFCell price = headerRow.createCell(3);
-			price.setCellValue("Giá nhập");
-			price.setCellStyle(headerCellStyle);
-
-			XSSFCell priceSell = headerRow.createCell(4);
-			priceSell.setCellValue("Giá bán");
-			priceSell.setCellStyle(headerCellStyle);
-
-			XSSFCell createdAt = headerRow.createCell(5);
-			createdAt.setCellValue("Ngày tạo");
-			createdAt.setCellStyle(headerCellStyle);
-
-			XSSFCell modifiedAt = headerRow.createCell(6);
-			modifiedAt.setCellValue("Ngày sửa");
-			modifiedAt.setCellStyle(headerCellStyle);
-
-			XSSFCell totalSold = headerRow.createCell(7);
-			totalSold.setCellValue("Đã bán");
-			totalSold.setCellStyle(headerCellStyle);
-
-			if (!products.isEmpty()) {
-				for (int i = 0; i < products.size(); i++) {
-					Product product = products.get(i);
-					XSSFRow bodyRow = worksheet.createRow(i + 1);
-					XSSFCellStyle bodyCellStyle = workbook.createCellStyle();
-					bodyCellStyle.setFillForegroundColor(new XSSFColor(java.awt.Color.WHITE));
-
-					XSSFCell productIDValue = bodyRow.createCell(0);
-					productIDValue.setCellValue(product.getId());
-					productIDValue.setCellStyle(bodyCellStyle);
-
-					XSSFCell productNameValue = bodyRow.createCell(1);
-					productNameValue.setCellValue(product.getName());
-					productNameValue.setCellStyle(bodyCellStyle);
-
-					XSSFCell productBrandValue = bodyRow.createCell(2);
-					productBrandValue.setCellValue(product.getBrand().getName());
-					productBrandValue.setCellStyle(bodyCellStyle);
-
-					XSSFCell priceValue = bodyRow.createCell(3);
-					priceValue.setCellValue(product.getPrice());
-					priceValue.setCellStyle(bodyCellStyle);
-
-					XSSFCell priceSellValue = bodyRow.createCell(4);
-					priceSellValue.setCellValue(product.getSalePrice());
-					priceSellValue.setCellStyle(bodyCellStyle);
-
-					CreationHelper creationHelper = workbook.getCreationHelper();
-					CellStyle cellStyle = workbook.createCellStyle();
-					cellStyle.setDataFormat(creationHelper.createDataFormat().getFormat("dd/MM/yyyy HH:mm:ss"));
-
-					XSSFCell createdAtValue = bodyRow.createCell(5);
-					createdAtValue.setCellValue(product.getCreatedAt());
-					createdAtValue.setCellStyle(cellStyle);
-
-					XSSFCell updatedAtValue = bodyRow.createCell(6);
-					updatedAtValue.setCellValue(product.getModifiedAt());
-					updatedAtValue.setCellStyle(cellStyle);
-
-					XSSFCell totalSoldValue = bodyRow.createCell(7);
-					totalSoldValue.setCellValue(product.getTotalSold());
-					totalSoldValue.setCellStyle(bodyCellStyle);
-				}
-			}
-			workbook.write(fos);
-			return file + "\\" + fileName + xlsx;
-		} catch (Exception e) {
-			return null;
-		}
-	}
-
-	private void fileDownload(String fullPath, HttpServletResponse response, String fileName, String type) {
-		File file = new File(fullPath);
-		if (file.exists()) {
-			OutputStream os = null;
-			try (FileInputStream fis = new FileInputStream(file);) {
-				String mimeType = context.getMimeType(fullPath);
-				response.setContentType(mimeType);
-				response.setHeader("content-disposition", "attachment; filename=" + fileName + "." + type);
-				os = response.getOutputStream();
-				byte[] buffer = new byte[BUFFER_SIZE];
-				int bytesRead = -1;
-				while ((bytesRead = fis.read(buffer)) != -1) {
-					os.write(buffer, 0, bytesRead);
-				}
-				Files.delete(file.toPath());
-			} catch (Exception e) {
-				log.error("Can't download file, detail: {}", e.getMessage());
-			} finally {
-				if (os != null) {
-					try {
-						os.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		}
 	}
 }
